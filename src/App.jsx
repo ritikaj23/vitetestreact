a5bc52c02b26795ea00a29e54afb048bb2d46523
ritikaj23 <ritikaj@skillup.online>
2024-05-17 05:12:05 -0400

    modifiedcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Navbar from './Components/Nav_bar/Navbar';
+import Login from './Components/Login/Login';
 function App() {
 
     return (
@@ -15,6 +16,7 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path='/' element={<Landing_Page/>}></Route>
+                <Route path='/login' element={<Login/>}/> 
             </Routes>
         </BrowserRouter>
 
